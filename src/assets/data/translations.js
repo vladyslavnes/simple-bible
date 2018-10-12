6bc501e21667f6fc9708bd4c049936ece75a8d74
Vladyslav Nesterenko <easterenko@gmail.com>
2018-10-12 11:30:40 +0300

    MVP

---
+++ src/assets/data/translations.js
@@ -0,0 +1,7 @@
+import KJV from './KJV.js'
+import RUSV from './RUSV.js'
+
+export default {
+  kjv: KJV,
+  rusv: RUSV
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import TRANSLATIONS from '../../assets/data/translations.js'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  translations = []
+  currentTranslation: any
+  currentBook: any
+  currentChapter: any
+  nightMode = false
 
   constructor(public navCtrl: NavController) {
+    for (let translationName in TRANSLATIONS) {
+      const t = { books: [...TRANSLATIONS[translationName]], name: translationName }
+      this.translations[translationName] = t
+    }
 
+    this.currentTranslation = 'kjv'
+    this.currentBook = 'Genesis'
+    this.currentChapter = 0
   }
 
+
+
+  public getTranslations() {
+    console.log(this.translations)
+    return Object.keys(this.translations)
+  }
+
+  public getChapters(bookName) {
+    const allBooks = this.translations[this.currentTranslation].books
+    for (let i = 0; i < allBooks.length; i++) {
+      if (allBooks[i].name === bookName) {
+        return allBooks[i].chapters
+      }
+    }
+    return null
+  }
+
+  public getChapter(bookName, chapterNum) {
+    return this.getChapters(bookName)[chapterNum]
+  }
+
+  public chaptersNumbers(bookName) {
+    let populatedNumbers = []
+    for (let i = 0; i < this.getChapters(bookName).length; i++) {
+      populatedNumbers.push(i)
+    }
+    return populatedNumbers
+  }
+
+  public switchNightMode() {
+    this.nightMode = !this.nightMode
+  }
 }
